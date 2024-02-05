8e1636825e7b71880ae351aa0f68cf19dd413667
Hristo Sivov <106160554+hsivov@users.noreply.github.com>
2024-02-05 18:47:02 +0200

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,49 @@
+variable "resource_group_name" {
+  type = string
+  description = "Resource group name in Azure"
+}
+
+variable "resource_group_location" {
+  type = string
+  description = "Resource group location in Azure"
+}
+
+variable "app_service_plan" {
+  type = string
+  description = "Service plan in Azure"
+}
+
+variable "app_service_name" {
+  type = string
+  description = "App service name in Azure"
+}
+
+variable "sql_server_name" {
+  type = string
+  description = "SQL server name in Azure"
+}
+
+variable "sql_database_name" {
+  type = string
+  description = "Database name in Azure"
+}
+
+variable "sql_admin_login" {
+  type = string
+  description = "Admin login"
+}
+
+variable "sql_admin_password" {
+  type = string
+  description = "Admin password"
+}
+
+variable "firewall_rule_name" {
+  type = string
+  description = "Firewall rule name"
+}
+
+variable "repo_URL" {
+  type = string
+  description = "GitHub repository URL"
+}

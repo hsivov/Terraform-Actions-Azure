1262e05def68bca4285e6909084fbed28a697187
Hristo Sivov <106160554+hsivov@users.noreply.github.com>
2024-02-07 19:07:59 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,49 +1,49 @@
 variable "resource_group_name" {
-  type = string
+  type        = string
   description = "Resource group name in Azure"
 }
 
 variable "resource_group_location" {
-  type = string
+  type        = string
   description = "Resource group location in Azure"
 }
 
 variable "app_service_plan" {
-  type = string
+  type        = string
   description = "Service plan in Azure"
 }
 
 variable "app_service_name" {
-  type = string
+  type        = string
   description = "App service name in Azure"
 }
 
 variable "sql_server_name" {
-  type = string
+  type        = string
   description = "SQL server name in Azure"
 }
 
 variable "sql_database_name" {
-  type = string
+  type        = string
   description = "Database name in Azure"
 }
 
 variable "sql_admin_login" {
-  type = string
+  type        = string
   description = "Admin login"
 }
 
 variable "sql_admin_password" {
-  type = string
+  type        = string
   description = "Admin password"
 }
 
 variable "firewall_rule_name" {
-  type = string
+  type        = string
   description = "Firewall rule name"
 }
 
 variable "repo_URL" {
-  type = string
+  type        = string
   description = "GitHub repository URL"
 }
